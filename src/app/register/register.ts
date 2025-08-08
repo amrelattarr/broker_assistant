0c97050229cec6767cd971ea3f51e4fa3748600e
dinaahmed0 <dinahmed1805@gmail.com>
2025-08-08 14:45:12 +0300

    regex

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule,AbstractControl, ValidationErrors } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css'],
 })
 export class Register {
+ registrationForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.createForm();
+  }
+
+
+  private emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  private passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  private usernameRegex = /^[a-zA-Z0-9_]{4,20}$/;
+
+
+    createForm() {
+    this.registrationForm = this.fb.group({
+      username: ['', [Validators.required, Validators.pattern(this.usernameRegex)]],
+      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+      password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+      confirmPassword: ['', Validators.required]
+    }, { 
+      validators: this.passwordMatchValidator 
+    });
+  }
+
+  passwordMatchValidator = (form: FormGroup) => {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form Submitted', this.registrationForm.value);
+      this.router.navigate(['/home']);
+    }
+  }
 }
+
+
